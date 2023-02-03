29f727acef0efa670583b7bf8111baf70345cfb9
duongpokee <97675279+duongpokee@users.noreply.github.com>
2023-02-03 13:13:13 +0700

    Update run1.py

--- run1.py
+++ run1.py
@@ -1,7 +1,7 @@
 import os
 
 def run():
-    code = 'docker run traffmonetizer/cli exit docker run traffmonetizer/cli start accept --token Rfh4oFnQAovccm4KZKOX1bMbUOsz/1k0ua8wJAdJESM= --device-name ' +  Ssever
+    code = 'docker run traffmonetizer/cli start accept --token Rfh4oFnQAovccm4KZKOX1bMbUOsz/1k0ua8wJAdJESM= --device-name ' +  Ssever
     os.system(code)
 
 Ssever = input('Nhap Sever: ')
