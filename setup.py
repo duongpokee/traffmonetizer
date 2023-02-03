e29a9bf64bffee80c9208ef42169179616886957
duongpokee <97675279+duongpokee@users.noreply.github.com>
2023-02-03 20:51:16 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def setup():
     os.system('sudo apt install apt-transport-https ca-certificates curl software-properties-common')
     os.system('sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"')
     os.system('sudo apt update')
-    os.system('sudo apt install docker-ce')
+    os.system('sudo apt install docker')
     os.system('docker pull traffmonetizer/cli:latest')
 def sever():
     global sever
